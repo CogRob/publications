4b367b2048cd287d0b8baf1f9644437b224b1ebf
ruffsl <roxfoxpox@gmail.com>
2019-10-11 15:38:33 -0700

    Move tidy script

---
+++ .scripts/bibtidy.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+bibtex-tidy "$@" \
+    --curly \
+    --numeric \
+    --space 13 \
+    --align \
+    --sort \
+    --strip-enclosing-braces \
+    --sort-fields \
+        address \
+        author \
+        booktitle \
+        category \
+        copyright \
+        day \
+        doi \
+        isbn \
+        issn \
+        journal \
+        location \
+        metadata \
+        month \
+        note \
+        number \
+        on \
+        pages \
+        publisher \
+        series \
+        shorttitle \
+        title \
+        url \
+        urldate \
+        volume \
+        year \
+    --strip-comments
