8402e01e94d75d8e7a4d01c30bf2eca98635af55
Anwesan Pal <31932995+anwesanpal@users.noreply.github.com>
2019-10-10 20:22:53 -0700

    Make Python3 compatible

--- bibparser.py
+++ bibparser.py
@@ -8,18 +8,19 @@ complete_entries = []
 all_bibs = ['conf-papers.bib', 'articles.bib', 'journal-issues.bib', 'reports.bib']
 
 for filename in all_bibs:
-    with open(filename) as bibtex_file:
-        bib_database = bibtexparser.load(bibtex_file)
+    parser = bibtexparser.bparser.BibTexParser(common_strings=True)
+    with open(filename, encoding='utf-8') as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file, parser=parser)
 
     entries = bib_database.entries
 
     for entry in entries:
         if(filename == 'conf-papers.bib'):
-            entry[unicode('type')] = unicode('Conference')
+            entry['type'] = 'Conference'
         elif(filename == 'articles.bib' or filename == 'journal-issues.bib'):
-            entry[unicode('type')] = unicode('Journal')
+            entry['type'] = 'Journal'
         elif(filename == 'reports.bib'):
-            entry[unicode('type')] = unicode('Report')
+            entry['type'] = 'Report'
         else:
             print("Unknown filename!")
             break
@@ -30,8 +31,7 @@ db = BibDatabase()
 db.entries = complete_entries
 
 writer = BibTexWriter()
-# writer.order_entries_by = ('year', 'month', 'type')
 writer.order_entries_by = ('author')
 
-with open('publications.bib', 'w') as bibfile:
+with open('publications.bib', 'w', encoding='utf-8') as bibfile:
     bibtexparser.dump(db, bibfile, writer)
